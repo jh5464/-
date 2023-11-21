1060208c651a6ed709d0b2c9cbbfcd6ceec9e401
jh5464 <135962005+jh5464@users.noreply.github.com>
2023-11-21 20:53:59 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,30 +1,44 @@
-﻿using System.Collections;
-using static System.Net.Mime.MediaTypeNames;
-
-namespace 队列
+﻿namespace 找k
 {
     internal class Program
     {
+        public static int[] FindMaxNumbers(int[] nums, int k)
+        {
+            if (k <= 0 || nums.Length == 0)
+                return new int[0];
+
+            int[] result = new int[k];
+
+            // 遍历数组，找到最大的 K 个数
+            for (int i = 0; i < k; i++)
+            {
+                int maxIndex = 0;
+                for (int j = 1; j < nums.Length; j++)
+                {
+                    if (nums[j] > nums[maxIndex])
+                        maxIndex = j;
+                }
+
+                result[i] = nums[maxIndex];
+                nums[maxIndex] = int.MinValue;  // 将已找到的最大数替换为最小值，以便下次循环不再考虑它
+            }
+
+            return result;
+        }
         static void Main(string[] args)
         {
-            Queue queue = new Queue();
-            //增加
-            queue.Enqueue(1);
-            queue.Enqueue("123");
-            queue.Enqueue(1.5f);
-            //取
-            object a = queue.Dequeue();
-            Console.WriteLine(a);
-            //查看
-            object b = queue.Peek();
-            Console.WriteLine(b);
-            if(queue.Contains(b))
-            {
-                Console.WriteLine("队列存在1.5f");
-            }
-           
-        }
-       
+            int[] nums = { 4, 2, 9, 7, 5, 8 };
+            int k = 3;
 
+            int[] result = FindMaxNumbers(nums, k);
+
+            Console.WriteLine("Max K Numbers:");
+            foreach (int num in result)
+            {
+                Console.WriteLine(num);
+            }
+
+            
+        }
     }
 }
